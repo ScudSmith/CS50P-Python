7981b6538e157b49e2ef28540fb9230f3cf4a68d
Sean Smith <scudsmith@gmail.com>
2024-12-04 15:31:02 -0500

    Tested utility to reformat files

--- Lesson 5/generate.py
+++ Lesson 5/generate.py
@@ -1,29 +1,28 @@
 # Will import all of random module
 import random
 
-#Will only import choice function
-#from random import choice
+# Will only import choice function
+# from random import choice
 
-#If importing whole module, have to fully 
-#qualify function call
+# If importing whole module, have to fully
+# qualify function call
 coin = random.choice(["heads", "tails"])
 
-#if importing only the function, can streamline
-#function call
-#coin = choice(["heads", "tails"])
+# if importing only the function, can streamline
+# function call
+# coin = choice(["heads", "tails"])
 
 print(coin)
 
-#Generate random number between the two 
-#parameters, inclusive
+# Generate random number between the two
+# parameters, inclusive
 number = random.randint(1, 10)
 print(number)
 
-#shuffle changes the order of the items
-#in the list
-cards =["jack", "queen", "king"]
+# shuffle changes the order of the items
+# in the list
+cards = ["jack", "queen", "king"]
 random.shuffle(cards)
 
 for card in cards:
     print(card)
-
